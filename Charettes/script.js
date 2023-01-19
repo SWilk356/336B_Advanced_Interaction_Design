45233e8507c3f7cd25dc0fa86ae5b758a2288c9a
Sarah Wilkinson <sarahwlknsn923@gmail.com>
2023-01-19 15:47:46 -0600

    setup for individual box calls

---
+++ Charettes/script.js
@@ -0,0 +1,319 @@
+$(document).on('click', '.cell', function() {
+    switch (this.id) {
+        case "cell-0":
+            console.log("cell 0:");
+;           break;
+        case "cell-1":
+            console.log("cell 1 clicked");
+            break;
+        case "cell-2":
+            console.log("cell 2 clicked");
+            break;
+        case "cell-3":
+            console.log("cell 3 clicked");
+            break;
+        case "cell-4":
+            console.log("cell 4 clicked");
+            break;
+        case "cell-5":
+            console.log("cell 5 clicked");
+            break;
+        case "cell-6":
+            console.log("cell 6 clicked");
+            break;
+        case "cell-7":
+            console.log("cell 7 clicked");
+            break;
+        case "cell-8":
+            console.log("cell 8 clicked");
+            break;
+        case "cell-9":
+            console.log("cell 9 clicked");
+            break;
+        case "cell-10":
+            console.log("cell 10 clicked");
+            break;
+        case "cell-11":
+            console.log("cell 11 clicked");
+            break;
+        case "cell-12":
+            console.log("cell 12 clicked");
+            break;
+        case "cell-13":
+            console.log("cell 13 clicked");
+            break;
+        case "cell-14":
+            console.log("cell 14 clicked");
+            break;
+        case "cell-15":
+            console.log("cell 15 clicked");
+            break;
+        case "cell-16":
+            console.log("cell 16 clicked");
+            break;
+        case "cell-17":
+            console.log("cell 17 clicked");
+            break;
+        case "cell-18":
+            console.log("cell 18 clicked");
+            break;
+        case "cell-19":
+            console.log("cell 19 clicked");
+            break;
+        case "cell-20":
+            console.log("cell 20 clicked");
+            break;
+        case "cell-21":
+            console.log("cell 21 clicked");
+            break;
+        case "cell-22":
+            console.log("cell 22 clicked");
+            break;
+        case "cell-23":
+            console.log("cell 23 clicked");
+            break;
+        case "cell-24":
+            console.log("cell 24 clicked");
+            break;
+        case "cell-25":
+            console.log("cell 25 clicked");
+            break;
+        case "cell-26":
+            console.log("cell 26 clicked");
+            break;
+        case "cell-27":
+            console.log("cell 27 clicked");
+            break;
+        case "cell-28":
+            console.log("cell 28 clicked");
+            break;
+        case "cell-29":
+            console.log("cell 29 clicked");
+            break;
+        case "cell-30":
+            console.log("cell 30 clicked");
+            break;
+        case "cell-31":
+            console.log("cell 31 clicked");
+            break;
+        case "cell-32":
+            console.log("cell 32 clicked");
+            break;
+        case "cell-33":
+            console.log("cell 33 clicked");
+            break;
+        case "cell-34":
+            console.log("cell 34 clicked");
+            break;
+        case "cell-35":
+            console.log("cell 35 clicked");
+            break;
+        case "cell-36":
+            console.log("cell 36 clicked");
+            break;
+        case "cell-37":
+            console.log("cell 37 clicked");
+            break;
+        case "cell-38":
+            console.log("cell 38 clicked");
+            break;
+        case "cell-39":
+            console.log("cell 39 clicked");
+            break;
+        case "cell-40":
+            console.log("cell 40 clicked");
+            break;
+        case "cell-41":
+            console.log("cell 41 clicked");
+            break;
+        case "cell-42":
+            console.log("cell 42 clicked");
+            break;
+        case "cell-43":
+            console.log("cell 43 clicked");
+            break;
+        case "cell-44":
+            console.log("cell 44 clicked");
+            break;
+        case "cell-45":
+            console.log("cell 45 clicked");
+            break;
+        case "cell-46":
+            console.log("cell 46 clicked");
+            break;
+        case "cell-47":
+            console.log("cell 47 clicked");
+            break;
+        case "cell-48":
+            console.log("cell 48 clicked");
+            break;
+        case "cell-49":
+            console.log("cell 49 clicked");
+            break;
+        case "cell-50":
+            console.log("cell 50 clicked");
+            break;
+        case "cell-51":
+            console.log("cell 51 clicked");
+            break;
+        case "cell-52":
+            console.log("cell 52 clicked");
+            break;
+        case "cell-53":
+            console.log("cell 53 clicked");
+            break;
+        case "cell-54":
+            console.log("cell 54 clicked");
+            break;
+        case "cell-55":
+            console.log("cell 55 clicked");
+            break;
+        case "cell-56":
+            console.log("cell 56 clicked");
+            break;
+        case "cell-57":
+            console.log("cell 57 clicked");
+            break;
+        case "cell-58":
+            console.log("cell 58 clicked");
+            break;
+        case "cell-59":
+            console.log("cell 59 clicked");
+            break;
+        case "cell-60":
+            console.log("cell 60 clicked");
+            break;
+        case "cell-61":
+            console.log("cell 61 clicked");
+            break;
+        case "cell-62":
+            console.log("cell 62 clicked");
+            break;
+        case "cell-63":
+            console.log("cell 63 clicked");
+            break;
+        case "cell-64":
+            console.log("cell 64 clicked");
+            break;
+        case "cell-65":
+            console.log("cell 65 clicked");
+            break;
+        case "cell-66":
+            console.log("cell 66 clicked");
+            break;
+        case "cell-67":
+            console.log("cell 67 clicked");
+            break;
+        case "cell-68":
+            console.log("cell 68 clicked");
+            break;
+        case "cell-69":
+            console.log("cell 69 clicked");
+            break;
+        case "cell-70":
+            console.log("cell 70 clicked");
+            break;
+        case "cell-71":
+            console.log("cell 71 clicked");
+            break;
+        case "cell-72":
+            console.log("cell 72 clicked");
+            break;
+        case "cell-73":
+            console.log("cell 73 clicked");
+            break;
+        case "cell-74":
+            console.log("cell 74 clicked");
+            break;
+        case "cell-75":
+            console.log("cell 75 clicked");
+            break;
+        case "cell-76":
+            console.log("cell 76 clicked");
+            break;
+        case "cell-77":
+            console.log("cell 77 clicked");
+            break;
+        case "cell-78":
+            console.log("cell 78 clicked");
+            break;
+        case "cell-79":
+            console.log("cell 79 clicked");
+            break;
+        case "cell-80":
+            console.log("cell 80 clicked");
+            break;
+        case "cell-81":
+            console.log("cell 81 clicked");
+            break;
+        case "cell-82":
+            console.log("cell 82 clicked");
+            break;
+        case "cell-83":
+            console.log("cell 83 clicked");
+            break;
+        case "cell-84":
+            console.log("cell 84 clicked");
+            break;
+        case "cell-85":
+            console.log("cell 85 clicked");
+            break;
+        case "cell-86":
+            console.log("cell 86 clicked");
+            break;
+        case "cell-87":
+            console.log("cell 87 clicked");
+            break;
+        case "cell-88":
+            console.log("cell 88 clicked");
+            break;
+        case "cell-89":
+            console.log("cell 89 clicked");
+            break;
+        case "cell-90":
+            console.log("cell 90 clicked");
+            break;
+        case "cell-91":
+            console.log("cell 91 clicked");
+            break;
+        case "cell-92":
+            console.log("cell 92 clicked");
+            break;
+        case "cell-93":
+            console.log("cell 93 clicked");
+            break;
+        case "cell-94":
+            console.log("cell 94 clicked");
+            break;
+        case "cell-95":
+            console.log("cell 95 clicked");
+            break;
+        case "cell-96":
+            console.log("cell 96 clicked");
+            break;
+        case "cell-97":
+            console.log("cell 97 clicked");
+            break;
+        case "cell-98":
+            console.log("cell 98 clicked");
+            break;
+        case "cell-99":
+            activate_gravity();
+            break;
+        case "cell-100":
+            console.log("cell 100 clicked");
+            break;
+
+    }
+})
+
+function activate_gravity() {
+    javascript:var script = document.createElement("script"); 
+    script.src="http://gravityscript.googlecode.com/svn/trunk/gravityscript.js";
+    document.body.appendChild(script);
+    void(0);
+}
+
+function move_right(cell_id) {
+    
+}
