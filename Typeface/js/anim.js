7cc1395abe802c22e1cd86d7ae47f002bf2528ed
Sarah Wilkinson <sarahwlknsn923@gmail.com>
2023-02-05 21:12:04 -0600

    add animations
    added bonfire animation
    working on adapting wood burning animation

--- Typeface/js/anim.js
+++ Typeface/js/anim.js
@@ -1,36 +1,26 @@
-var fire = document.getElementsByClassName("fireplace");
-var sparks = fire[0].getElementsByTagName("main");
+var bonfire = document.getElementsByClassName("fireplace");
+var sparks = bonfire[0].getElementsByTagName("main");
 
 var littleFire = document.getElementsByClassName("fireplace__flame");
 var bigFire = document.getElementsByClassName("fireplace__flame_big");
 
 $(document).on( "mousedown", '.fireplace', function() {
     //set fire animation to running
-    setFireAnimation(0);
+    setFireAnimation("running");
 } )
 
 $(document).on( "mouseup", '.fireplace', function() {
     //set fire animation to pause
-    setFireAnimation(1);
+    setFireAnimation("paused");
 
 } )
 
-function setFireAnimation(pause) {
-    if(pause) {
-        littleFire[0].style.animationPlayState = "paused";
-        bigFire[0].style.animationPlayState = "paused";
-    
-        sparks[0].style.animationPlayState = "paused";
-        sparks[1].style.animationPlayState = "paused";
-        sparks[2].style.animationPlayState = "paused";
-        sparks[3].style.animationPlayState = "paused";
-    } else {
-        littleFire[0].style.animationPlayState = "running";
-        bigFire[0].style.animationPlayState = "running";
-    
-        sparks[0].style.animationPlayState = "running";
-        sparks[1].style.animationPlayState = "running";
-        sparks[2].style.animationPlayState = "running";
-        sparks[3].style.animationPlayState = "running";
-    }
+function setFireAnimation(state) {
+    littleFire[0].style.animationPlayState = state;
+    bigFire[0].style.animationPlayState = state;
+
+    sparks[0].style.animationPlayState = state;
+    sparks[1].style.animationPlayState = state;
+    sparks[2].style.animationPlayState = state;
+    sparks[3].style.animationPlayState = state;
 }
