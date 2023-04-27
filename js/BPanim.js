aae7033af7f8989edfba66701311f75dda83e771
Sarah Wilkinson <sarahwlknsn923@gmail.com>
2023-04-27 07:16:02 -0500

    circle algorithm works
    thank god
    need to make fun shapes now

--- js/BPanim.js
+++ js/BPanim.js
@@ -100,23 +100,22 @@ function expandCircle(firstPiece) {
     //set first four quad leads
     findFirstFour(firstPiece);
 
+    //set expansion limits so that circle does not wrap around grid
     let thisID = getPieceID(firstPiece);
     let rowPos = thisID % (limitCol+1);
-    let columnTopLimit = thisID + Math.trunc(limitCol+1 - rowPos);
-    let columnBottomLimit = thisID - rowPos;
+    let q1Limit = thisID + Math.trunc(limitCol+1 - rowPos);
+    let q3Limit = thisID - rowPos;
+    let q2Limit = q3Limit - limitCol-1;
+    let q4Limit = q1Limit + limitCol+1;
 
-    console.log(columnTopLimit);
-    console.log(columnBottomLimit);
-
-    //for loop where you call each quadrant
     let delay = 0; //animation delay value; increments each time after all four quadrants have been expanded
     let countFinishedQuads = 0; //counts number of finished quadrants
-    let timeout = 0; //debug timeout in case while loop is broken
     let outOfPieces = 0; //check if this quadrant is finished
 
     //burn first five
     for (let i = 0; i < 5; i++) {
-        burnOnePiece(delay, quadLeads[i]);
+        //console.log(quadLeads[i])
+        burnOnePiece(delay, 0, quadLeads[i]);
         quadLeads[i].removeClass('burn');
     }
 
@@ -125,38 +124,56 @@ function expandCircle(firstPiece) {
     while (countFinishedQuads < 4) {
         //since while loop has proceeded past initial check, it must mean that the number of finished quads is not yet four so reset the count
         countFinishedQuads = 0;
+        //increment delay to have staggered animation effect between rounds
+        delay++;
 
-        for (let q = 1; q <=4; q++) {
-            //expandQuadrants returns 0 if successful round of expansion. returns 1 if quadrant is completely expanded.
+        for (let q = 1; q <= 4; q++) {
             outOfPieces = 0;
 
-            expansionLimit = (q==1 || q==4) ? columnBottomLimit : columnTopLimit;
-            outOfPieces = expandQuadrants(q, expansionLimit, delay, quadLeads[q]);
-            //console.log("expand output: " + outOfPieces);
+            if (!quadLeads[q]) {
+                continue;
+            }
+
+            //have fun with shapes
+            if (w < 480) {
+    
+            } else if (w < 768) {
+                
+            } else if (w < 1024) {
+                
+            } else if (w < 1500) {
+                
+            } else {
+                if (q == 2 || q == 3) {
+                    continue;
+                }
+                
+            }
+
+            switch (q) {
+                case 1: expansionLimit = q1Limit; break;
+                case 2: expansionLimit = q2Limit; break;
+                case 3: expansionLimit = q3Limit; break;
+                case 4: expansionLimit = q4Limit; break;
+            }
+
+
+            outOfPieces = expandQuadrants(q, expansionLimit, delay, quadLeads[q]);//expandQuadrants returns 0 if successful round of expansion. returns 1 if quadrant is completely expanded.
 
             if (outOfPieces) {
                 countFinishedQuads++;
             }
         }
-
-        //increment delay to have staggered animation effect between rounds
-        delay++;
-
-        //------------
-        //debug
-        timeout++;
-        
-        if (timeout > 10000) {
-            alert("loop timeout");
-            break;
-        }
-        //------------
     }
 }
 
 //given a quadrant number and the current node, expand outward
 function expandQuadrants(quadrant, expansionLimit, delay, currPiece) {
     let horizFactor = 1; //horizontal expansion direction changes according which quad
+    //set up like cartesian coordinate plane. If q1 or q2, expand upward. If q3 or q4, expand downward. If q2 or q3, expands to left. If q1 or q4, expand to right.
+    if (quadrant == 2 || quadrant == 3) {
+        horizFactor = -1; 
+    }
     
     //STEP 1:
     //burn the current quadLead piece
@@ -167,29 +184,36 @@ function expandQuadrants(quadrant, expansionLimit, delay, currPiece) {
 
     //STEP 2:
     //get the next quadLead piece
-
+    let thisID;
     //prepare to get next piece id
-    let thisID = parseInt(currPiece.attr("id").substring(1));
-    //set up like cartesian coordinate plane. If q1 or q2, expand upward. If q3 or q4, expand downward. If q2 or q3, expands to left. If q1 or q4, expand to right.
-    if (quadrant == 2 || quadrant == 3) {
-        horizFactor = setShapeBasedOnWindow(); 
+    if (currPiece.attr("id")) {
+        thisID = parseInt(currPiece.attr("id").substring(1));
+    } else {
+        return 1;
     }
 
     //get next id
     nextHorizID = thisID + horizFactor;
-    
+
     //check if this piece is past expansion limits i.e. has it exceeded the original piece's row?
     if (quadrant == 1 || quadrant == 4) {
-        if (nextHorizID < expansionLimit) {
+        
+        if (nextHorizID > expansionLimit) {
+            console.log("1 or 4 exceeded");
             return 1;
         }
     } else {
-        if (nextHorizID > expansionLimit) {
+        if (nextHorizID < expansionLimit) {
+            console.log("2 or 3 exceeded");
             return 1;
         }
     }
 
-    nextHorizPiece = $("#p" + nextHorizID);
+    let nextHorizPiece = $("#p" + nextHorizID);
+    if (!nextHorizPiece.attr("id")) {
+        //console.log(nextHorizPiece);
+        return 1;
+    }
 
     //set this next piece as the next quad lead
     quadLeads[quadrant] = nextHorizPiece;
@@ -200,26 +224,27 @@ function expandQuadrants(quadrant, expansionLimit, delay, currPiece) {
 
 //recursive function to cascade up or down depending on quadrant
 function cascadeQuadrant(quadrant, horizFactor, delay, currPiece) {
-    
     let nextVertPiece = getVerticalPiece(quadrant, currPiece);
 
     //if this is a valid piece i.e. we haven't run out of pieces to add, then burn it. otherwise, return.
-    if (!nextVertPiece.length) {
-        console.log("empty");
+    if (!nextVertPiece) {
         return;
     }
 
     burnOnePiece(delay, nextVertPiece);
     nextVertPiece.removeClass('burn');
 
+    let diagonalID;
     //get diagonal
-    let diagonalID = (quadrant == 2 || quadrant == 3) ? parseInt(nextVertPiece.attr("id").substring(1)) + horizFactor : parseInt(nextVertPiece.attr("id").substring(1)) - horizFactor;
-
-    //if there is another diagonal node, repeat the above process on that node
-    //THIS CURRENTLY DOES NOT STOP AT THE QUADRANT EDGE BUT KEEPS GOING, SEE IF THAT IS ACTUALY A PROBLEM BECAUSE IT WILL DOULBE ADD WITH OTHER QUADRANTS
-    if ($("#p" + diagonalID).length) {
+    if (nextVertPiece.attr("id")) {
+        diagonalID = (quadrant == 2 || quadrant == 3) ? parseInt(nextVertPiece.attr("id").substring(1)) + horizFactor : parseInt(nextVertPiece.attr("id").substring(1)) - horizFactor;
         cascadeQuadrant(quadrant, horizFactor, delay, $("#p" + diagonalID));
     }
+    
+    //if there is another diagonal node, repeat the above process on that node
+    //THIS CURRENTLY DOES NOT STOP AT THE QUADRANT EDGE BUT KEEPS GOING, SEE IF THAT IS ACTUALY A PROBLEM BECAUSE IT WILL DOULBE ADD WITH OTHER QUADRANTS
+
+    return;
 }
 
 function setShapeBasedOnWindow() {
@@ -244,7 +269,13 @@ function burnOnePiece(delay, currPiece) {
 //helper function to cascadeQuadrant
 //get the next vertical piece, whether that is up or down
 function getVerticalPiece(quadrant, currPiece) {
-    let thisID = parseInt(currPiece.attr("id").substring(1));
+    let thisID = parseInt(currPiece.attr("id"));
+    if (currPiece.attr("id")) {
+        thisID = parseInt(currPiece.attr("id").substring(1));
+    } else {
+        return null;
+    }
+
     let nextVertID;
     
     if (quadrant < 3) {
